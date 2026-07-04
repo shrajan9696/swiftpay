bd65a09e59217d7b350c7e08ff8f352f60309500
Shrajan <jainshrajan482@gmail.com>
2026-07-04 15:56:09 +0530

    few changes

--- ledger-service/src/main/resources/data.sql
+++ ledger-service/src/main/resources/data.sql
@@ -1,7 +1,9 @@
 INSERT INTO accounts(id, account_number, balance, currency, holder_name)
 VALUES
-(101,'6748781873',1000,'INR','Shrajan Jain');
+(101,'6748781873',1000,'INR','Shrajan Jain')
+ON CONFLICT (id) DO NOTHING;
 
 INSERT INTO accounts(id, account_number, balance, currency, holder_name)
 VALUES
-(102, '915789758984',2000,'INR','Prashant Singh');
+(102, '915789758984',2000,'INR','Prashant Singh')
+ON CONFLICT (id) DO NOTHING;
--- transaction-gateway/src/main/java/transactiongateway/dto/PaymentRequest.java
+++ transaction-gateway/src/main/java/transactiongateway/dto/PaymentRequest.java
@@ -1,5 +1,6 @@
 package transactiongateway.dto;
 
+import io.swagger.v3.oas.annotations.media.Schema;
 import jakarta.validation.constraints.DecimalMin;
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.NotNull;
@@ -16,21 +17,41 @@ import java.math.BigDecimal;
 @Builder
 public class PaymentRequest {
 
+    @Schema(
+            description = "Unique transaction id",
+            example = "TXN5001"
+    )
     @NotBlank(message = "Transaction Id is required")
     private String transactionId;
 
+    @Schema(
+            description = "Sender Account Id",
+            example = "101"
+    )
     @NotNull(message = "Sender Id is required")
     @Positive(message = "Sender Id must be positive")
     private Long senderId;
 
+    @Schema(
+            description = "Receiver Account Id",
+            example = "102"
+    )
     @NotNull(message = "Receiver Id is required")
     @Positive(message = "Receiver Id must be positive")
     private Long receiverId;
 
+    @Schema(
+            description = "Transfer amount",
+            example = "250"
+    )
     @NotNull(message = "Amount is required")
     @DecimalMin(value = "0.01", message = "Amount must be greater than zero")
     private BigDecimal amount;
 
+    @Schema(
+            description = "Currency",
+            example = "INR"
+    )
     @NotBlank(message = "Currency is required")
     @Size(min = 3, max = 3, message = "Currency must be a 3-letter ISO code")
     private String currency;
